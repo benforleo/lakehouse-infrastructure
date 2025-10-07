f6d3bd40dfa462d7b3dbe9fcef401266bf8d93fa
Benjamin Forleo <40501242+benforleo@users.noreply.github.com>
2025-10-07 16:19:19 -0400

    ECS Cluster

--- src/aws/polaris/polaris.ts
+++ src/aws/polaris/polaris.ts
@@ -1,6 +1,13 @@
+import * as pulumi from "@pulumi/pulumi";
+import { PolarisECS } from './components/ecs'
+import {PolarisPostgresDB} from "./components/postgres";
+import * as ecr from "./components/ecr";
+
 
 export class Polaris {
-    constructor() {
-
+    constructor(config: pulumi.Config) {
+        new ecr.ECR();
+        new PolarisPostgresDB(config);
+        new PolarisECS();
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import * as dbt from './snowflake/dbt-config/dbt-config';
 import * as ecr from './aws/polaris/components/ecr';
-import {PolarisPostgresDB} from './aws/polaris/components/postgres';
+import { Polaris } from './aws/polaris/polaris';
 
 import * as pulumi from "@pulumi/pulumi";
 
 
-function main() {
 
+function main() {
     const config = new pulumi.Config();
-    new PolarisPostgresDB(config);
-    new ecr.ECR();
+
+    new Polaris(config)
     new dbt.SnowflakeDBTTrainingResources();
 }
 
