3c4d9a64a62a2e5e225bb1866327d5edf48a0306
Benjamin Forleo <40501242+benforleo@users.noreply.github.com>
2025-10-08 05:18:35 -0400

    Add log group

--- src/aws/polaris/components/ecs.ts
+++ src/aws/polaris/components/ecs.ts
@@ -1,7 +1,16 @@
 import * as aws from '@pulumi/aws'
+import * as awsx from '@pulumi/awsx'
+import {config} from "@pulumi/aws";
 
 export class PolarisECS {
     constructor() {
+
+        const logGroup = new aws.cloudwatch.LogGroup("PolarisLogGroup", {
+            name: "/aws-ecs/polaris",
+            retentionInDays: 7,
+            skipDestroy: false,
+            region: "us-east-1",
+        });
 
         const cluster = new aws.ecs.Cluster("PolarisCluster", {
             name: "polaris-cluster",
@@ -12,6 +21,26 @@ export class PolarisECS {
             }]
         });
 
+        // const taskDefinition = new aws.ecs.TaskDefinition("PolarisTask", {
+        //     family: 'polaris-task',
+        //     containerDefinitions: JSON.stringify({
+        //
+        //     }),
+        // });
 
+        // const taskDefinition = new awsx.ecs.FargateTaskDefinition("PolarisTask", {
+        //     container: {
+        //         image: '429414942599.dkr.ecr.us-east-1.amazonaws.com/polaris:1.1.0-incubating',
+        //         name: 'polaris'
+        //     },
+        //     cpu: '1024',
+        //     family: 'polaris-task',
+        //     logGroup: {
+        //         args: {}
+        //     }
+        //     memory: '2048',
+        //
+        //
+        // });
     }
 }
